f3c5c37b78201fd9bfbf760cf0ebdbd22ee8e694
Roshan Choxi <roshan.choxi@gmail.com>
2016-06-22 14:43:49 -0700

    remove solution

--- application.js
+++ application.js
@@ -1,14 +1,1 @@
-app = angular.module('Tasks', []);
-
-app.controller("TaskController", function($scope) {
-  $scope.newTask = null;
-  $scope.tasks   = [
-    { name: "Do the thing" },
-    { name: "Do another thing" }
-  ];
-
-  $scope.createTask = function(task) {
-    $scope.tasks.push(task);
-    $scope.newTask = null;
-  };
-});
+// application.js
