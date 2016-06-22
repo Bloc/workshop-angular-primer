823fb989af7a8678528c748ce22f026e4fd55f65
Roshan Choxi <roshan.choxi@gmail.com>
2016-06-22 14:07:48 -0700

    exercise 4

---
+++ test/exercise_4.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000;
+
+app.use(express.static("."));
+app.listen(port);
+
+var fs        = require('fs');
+var assert    = require('chai').assert;
+var Horseman  = require('node-horseman');
+
+describe("angular app", function() {
+  before(function(){
+    this.horseman = new Horseman();
+  });
+
+  it("displays multiple tasks", function() {
+    return this.horseman
+      .open('http://localhost:' + port)
+      .screenshot("screenshot.png")
+      .text("ul.tasks li")
+      .then(function(text) {
+        assert.match(text, /Do the thing/);
+        assert.match(text, /Do another thing/);
+      });
+  });
+
+  after(function() {
+    //this.horseman.close();
+    //this.server.close();
+  });
+})
