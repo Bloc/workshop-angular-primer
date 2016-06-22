0b30b9e48608a7c2b6e05343e88c12ae90671c17
Roshan Choxi <roshan.choxi@gmail.com>
2016-06-22 13:34:31 -0700

    exercise 1 spec

--- test/exercise_1.js
+++ test/exercise_1.js
@@ -3,10 +3,10 @@ var fs        = require('fs');
 var assert    = require('assert');
 var Horseman  = require('node-horseman');
 
-describe("angular initialization", function() {
+describe("angular script", function() {
   before(function(){
     this.horseman = new Horseman();
-    this.server  = http.createServer(function (req, res) {
+    this.server   = http.createServer(function (req, res) {
       var index = fs.readFileSync('index.html', {encoding: "utf8"});
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(index);
@@ -14,21 +14,14 @@ describe("angular initialization", function() {
     }).listen(3000);
   });
 
-  it("interpolates {{ 5 + 5 }}", function() {
+  it("loads the angular library", function() {
     return this.horseman
       .open('http://localhost:3000')
-      .text("h1")
-      .then(function(text) {
-        assert.equal(text, " 10 ");
-      });
-  });
-
-  it("has an HTML tag", function() {
-    return this.horseman
-      .open('http://localhost:3000')
-      .count("html")
-      .then(function(count) {
-        assert.equal(count, 1);
+      .evaluate(function(){
+        return angular;
+      })
+      .then(function(angular) {
+        assert.notDeepEqual(angular, null);
       });
   });
 
