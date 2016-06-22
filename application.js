a6582ea5f71e6758db480fdc1d824ee7e5f03709
Roshan Choxi <roshan.choxi@gmail.com>
2016-06-22 13:21:47 -0700

    implement simple todo list

--- application.js
+++ application.js
@@ -1,1 +1,10 @@
-console.log("Loaded application.js");
+app = angular.module('Tasks', []);
+app.controller("TaskController", function($scope) {
+  $scope.newTask = null;
+  $scope.tasks   = [];
+  
+  $scope.createTask = function(task) {
+    $scope.tasks.push(task);
+    $scope.newTask = null;
+  };
+});
